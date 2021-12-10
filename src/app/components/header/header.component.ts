e78a40f775efc0b8e34b1b80e79eb6244d5f85be
Vudang <danglegiavu@gmail.com>
2021-12-10 18:33:36 +0700

    add header module

---
+++ src/app/components/header/header.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
+})
+export class HeaderComponent implements OnInit {
+  title : string = 'Kanban Board';
+  constructor() { }
+
+
+  toogleAddTask(){
+    console.log('hehe')
+  }
+
+  ngOnInit(): void {
+  }
+
+}
