1f26a30f93ff950b662aad5fc5215aaf0bc66e09
Vudang <danglegiavu@gmail.com>
2021-12-10 18:33:15 +0700

    add tasks module

---
+++ src/mock/task/task.interface.ts
@@ -0,0 +1,6 @@
+export interface ITask {
+  id:number,
+  name: string,
+  day: string,
+  reminder:boolean
+}
---
+++ src/mock/task/task.mock.ts
@@ -0,0 +1,46 @@
+import { ITask } from "./task.interface";
+
+export const TASKS : ITask[] = [
+	{
+		id: 1,
+		name:'Meeting honey',
+		day:'May 5th at 2:30pm',
+		reminder:true
+	},
+	{
+		id: 2,
+		name:'Meeting uncle of honey',
+		day:'May 5th at 2:30pm',
+		reminder:true
+	},
+	{
+		id: 3,
+		name:'Meeting aunt of honey',
+		day:'May 5th at 2:30pm',
+		reminder:true
+	},
+	{
+		id: 4,
+		name:'Meeting mother of honey',
+		day:'May 5th at 2:30pm',
+		reminder:true
+	},
+	{
+		id: 5,
+		name:'Meeting father of honey',
+		day:'May 5th at 2:30pm',
+		reminder:true
+	},
+	{
+		id: 6,
+		name:'Meeting grandfather of honey',
+		day:'May 5th at 2:30pm',
+		reminder:true
+	},
+	{
+		id: 7,
+		name:'Meeting grandmother of honey',
+		day:'May 5th at 2:30pm',
+		reminder:true
+	}
+]
